0328a6945a7b1438417397d688c5865d54c96c59
stormytuna <stormytuna@outlook.com>
2022-10-15 12:22:55 +0100

    Update stormytunaUtils.cs

--- stormytunaUtils.cs
+++ stormytunaUtils.cs
@@ -15,7 +15,7 @@ namespace ThreatOfPrecipitation
     {
 
         /// <summary>Gets a list of NPCs within the range of that position</summary>
-        /// <param name="position">The position</param>
+        /// <param name="position">The position, should be the center of the search and usually the center of another entity</param>
         /// <param name="range">The range measured in units, 1 tile is 16 units</param>
         /// <returns>A list of NPCs within range of the position</returns>
         public static List<NPC> GetNearbyNPCs(Vector2 position, float range)
@@ -26,7 +26,7 @@ namespace ThreatOfPrecipitation
             for (int i = 0; i < Main.npc.Length; i++)
             {
                 NPC npc = Main.npc[i];
-                float distanceSquared = Vector2.DistanceSquared(position, npc.position);
+                float distanceSquared = Vector2.DistanceSquared(position, npc.Center);
                 if (distanceSquared <= rangeSquared)
                 {
                     npcs.Add(npc);
@@ -34,6 +34,37 @@ namespace ThreatOfPrecipitation
             }
 
             return npcs;
+        }
+
+        /// <summary>Gets the closest hostile NPC within the range of that position</summary>
+        /// <param name="position">The position, should be the center of the search and usually the center of another entity</param>
+        /// <param name="range">The range measured in units, 1 tile is 16 units</param>
+        /// <param name="careAboutLineOfSight">Whether the function should check Collision.CanHit</param>
+        /// <returns>Returns the closest NPC. Returns null if no NPC is found</returns>
+        public static NPC GetClosestEnemy(Vector2 position, float range, bool careAboutLineOfSight)
+        {
+            NPC closestNPC = null;
+            float rangeSquared = range * range;
+
+            for (int i = 0; i < Main.npc.Length; i++)
+            {
+                NPC npc = Main.npc[i];
+
+                if (!npc.active || npc.damage <= 0 || npc.CountsAsACritter || npc.friendly)
+                { 
+                    continue;
+                }
+
+                float distanceSquared = Vector2.DistanceSquared(position, npc.Center);
+                bool canSee = careAboutLineOfSight ? Collision.CanHit(position, 1, 1, npc.position, npc.width, npc.height) : true;
+                if (distanceSquared < rangeSquared && canSee)
+                {
+                    closestNPC = npc;
+                    rangeSquared = distanceSquared;
+                }
+            }
+
+            return closestNPC;
         }
     }
 }
